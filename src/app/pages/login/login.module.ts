d6d9810d0fef36a4376417b67455a3d8be215fea
Gustavo Brito <gugadb01@gmail.com>
2019-01-03 15:27:30 -0200

    Adding basic login flow

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+	MatCardModule,
+	MatButtonModule,
+	MatFormFieldModule,
+	MatInputModule,
+	MatSelectModule,
+	MatIconModule,
+	MatProgressSpinnerModule,
+
+} from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [LoginComponent],
 	imports: [
 		CommonModule,
 		MatButtonModule,
-		MatCheckboxModule,
-		FlexLayoutModule
+		FlexLayoutModule,
+		MatCardModule,
+		MatButtonModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatSelectModule,
+		MatIconModule,
+		MatProgressSpinnerModule,
+		FormsModule
 	]
 })
 export class LoginModule { }
