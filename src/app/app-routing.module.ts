56bee3fbed1ea26209911d7a148c06424b67ad21
Emerson Demetrio <emer.demetrio@gmail.com>
2019-01-03 10:24:58 -0200

    AMVP - Add Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LoggedOutGuard } from './guards/logged-out.guard';
 
-const routes: Routes = [];
+const routes: Routes = [{
+	path: '',
+	redirectTo: '/login',
+	pathMatch: 'full'
+}, {
+	path: 'login',
+	component: LoginComponent,
+	canActivate: [LoggedOutGuard]
+}];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
